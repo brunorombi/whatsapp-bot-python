290ab80ce3732ee95476fbc55218ab8459fc1d24
Bruno <113005942+brunorombi@users.noreply.github.com>
2025-02-24 23:22:42 -0300

    adjusting line spacing and formatting

--- wpp.bot.py
+++ wpp.bot.py
@@ -5,20 +5,16 @@ import time
 arquivo_excel = "teste.xlsx"  # Substituir pelo caminho da planilha
 planilha = pd.read_excel(arquivo_excel)
 
-
 planilha['Observação'] = planilha['Observação'].fillna('')  
 planilha['Observação'] = planilha['Observação'].astype(str)
-
 
 for index, row in planilha.iterrows():
     nome = row['Nome'].split()[0]  
     numero = str(row['Celular'])  
-
     
     if not numero.startswith("+"):
         numero = "+55" + numero  
 
-    
     mensagem = f"Oi, tudo bem {nome}, essa é uma mensagem de teste! 😊"
     
     try:
@@ -26,9 +22,7 @@ for index, row in planilha.iterrows():
         kit.sendwhatmsg_instantly(numero, mensagem, wait_time=10, tab_close=True)
         print(f"Mensagem enviada para {nome} ({numero})!")
 
-        
         planilha.at[index, 'Observação'] = "Mensagem Enviada"
-        
         
         time.sleep(5)
     except Exception as e:
